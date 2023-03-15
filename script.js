fbd6e69205210dc9f2104744f8fc66fb8e694170
Nilesssh <103926034+Nilesssh@users.noreply.github.com>
2023-03-15 17:17:37 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,7 +1,9 @@
 // complete the given function
 
-function palindrome(str){
+function palindrome(omg){
+	let str = omg.toLowerCase();
 	for(let i=0; i<str.length/2; i++){
+		
 		if(str[i]!==str[(str.length-1)-i]){
 			return false;
 		}
